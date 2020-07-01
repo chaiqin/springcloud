d04472dab013b08e900e40a7603de65ed18b2600
chq <17875511931@163.com>
2020-07-01 15:23:46 +0800

    'x'

--- cloud-provider-hystrix-payment8001/src/main/java/com/chq/springcloud/service/PaymentService.java
+++ cloud-provider-hystrix-payment8001/src/main/java/com/chq/springcloud/service/PaymentService.java
@@ -1,5 +1,7 @@
 package com.chq.springcloud.service;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
 import org.springframework.stereotype.Service;
 
 import java.util.concurrent.TimeUnit;
@@ -12,9 +14,17 @@ public class PaymentService {
     }
 
     //失败
+    @HystrixCommand(fallbackMethod = "paymentInfo_TimeOutHandler",commandProperties = {
+            @HystrixProperty(name = "execution.isolation.thread.timeoutInMilliseconds",value = "3000")
+    })
     public String paymentInfo_TimeOut(Integer id){
-        int timeNumber = 3;
+        int i = 1/0;
+        int timeNumber = 5;
         try { TimeUnit.SECONDS.sleep(timeNumber); }catch (Exception e) {e.printStackTrace();}
         return "线程池："+Thread.currentThread().getName()+"   paymentInfo_TimeOut,id：  "+id+"\t"+"呜呜呜"+" 耗时(秒)"+timeNumber;
+    }
+
+    public String paymentInfo_TimeOutHandler(Integer id){
+        return "/(TOT)/系统繁忙，请稍后再试,当前线程池名字"+Thread.currentThread().getName();
     }
 }
