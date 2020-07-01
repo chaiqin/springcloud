f01524abc7322a1ac27027ca7c67322836049a1c
chq <17875511931@163.com>
2020-07-01 17:12:48 +0800

    'x'

--- cloud-provider-hystrix-payment8001/src/main/java/com/chq/springcloud/service/PaymentService.java
+++ cloud-provider-hystrix-payment8001/src/main/java/com/chq/springcloud/service/PaymentService.java
@@ -15,11 +15,10 @@ public class PaymentService {
 
     //失败
     @HystrixCommand(fallbackMethod = "paymentInfo_TimeOutHandler",commandProperties = {
-            @HystrixProperty(name = "execution.isolation.thread.timeoutInMilliseconds",value = "3000")
+            @HystrixProperty(name = "execution.isolation.thread.timeoutInMilliseconds",value = "5000")
     })
     public String paymentInfo_TimeOut(Integer id){
-        int i = 1/0;
-        int timeNumber = 5;
+        int timeNumber = 3;
         try { TimeUnit.SECONDS.sleep(timeNumber); }catch (Exception e) {e.printStackTrace();}
         return "线程池："+Thread.currentThread().getName()+"   paymentInfo_TimeOut,id：  "+id+"\t"+"呜呜呜"+" 耗时(秒)"+timeNumber;
     }
