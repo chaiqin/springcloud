826498709fdbcef91ea3c443b9ab88754dcb9e18
chq <17875511931@163.com>
2020-07-03 19:25:17 +0800

    'x'

--- cloud-consumer-feign-hystrix-order80/src/main/java/com/chq/springcloud/controller/OrderHystrixController.java
+++ cloud-consumer-feign-hystrix-order80/src/main/java/com/chq/springcloud/controller/OrderHystrixController.java
@@ -1,6 +1,7 @@
 package com.chq.springcloud.controller;
 
 import com.chq.springcloud.service.PaymentHystrixService;
+import com.netflix.hystrix.contrib.javanica.annotation.DefaultProperties;
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
 import lombok.extern.slf4j.Slf4j;
@@ -13,6 +14,7 @@ import javax.annotation.Resource;
 
 @RestController
 @Slf4j
+@DefaultProperties(defaultFallback = "payment_Global_FallbackMethod")  //全局的
 public class OrderHystrixController {
 
     @Resource
@@ -28,10 +30,12 @@ public class OrderHystrixController {
         return result;
     }
     @GetMapping("/consumer/payment/hystrix/timeout/{id}")
-    @HystrixCommand(fallbackMethod = "paymentTimeOutFallbackMethod",commandProperties = {
-            @HystrixProperty(name = "execution.isolation.thread.timeoutInMilliseconds",value = "1500")
-    })
+//    @HystrixCommand(fallbackMethod = "paymentTimeOutFallbackMethod",commandProperties = {
+//            @HystrixProperty(name = "execution.isolation.thread.timeoutInMilliseconds",value = "1500")
+//    })
+    @HystrixCommand
     public String paymentInfo_TimeOut(@PathVariable("id") Integer id){
+        int a = 10/0;
         String result = paymentHystrixService.paymentInfo_TimeOut(id);
         log.info("*******result:"+result);
         return result;
@@ -40,5 +44,10 @@ public class OrderHystrixController {
     //兜底方法
     public String paymentTimeOutFallbackMethod(@PathVariable("id") Integer id){
         return "我是消费者80，对付支付系统繁忙请10秒钟后再试或者自己运行出错请检查自己,(┬＿┬)";
+    }
+
+    //下面是全局fallback方法
+    public String payment_Global_FallbackMethod(){
+        return "Global异常处理信息，请稍后再试,(┬＿┬)";
     }
 }
