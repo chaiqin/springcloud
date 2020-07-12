fcb65b098c3a382f33b5222ae3df55a3e9001d9e
chq <17875511931@163.com>
2020-07-12 22:43:10 +0800

    'sentinel降级'

--- cloudalibaba-sentinel-service8401/src/main/java/com/chq/springcloud/controller/FlowLimitController.java
+++ cloudalibaba-sentinel-service8401/src/main/java/com/chq/springcloud/controller/FlowLimitController.java
@@ -1,9 +1,13 @@
 package com.chq.springcloud.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.concurrent.TimeUnit;
+
 @RestController
+@Slf4j
 public class FlowLimitController
 {
     @GetMapping("/testA")
@@ -17,7 +21,13 @@ public class FlowLimitController
         return "------testB";
     }
 
-
+    @GetMapping("/testD")
+    public String testD()
+    {
+        try { TimeUnit.SECONDS.sleep(1); } catch (InterruptedException e) { e.printStackTrace(); }
+        log.info("testD 测试RT");
+        return "------testD";
+    }
 
 }
  
