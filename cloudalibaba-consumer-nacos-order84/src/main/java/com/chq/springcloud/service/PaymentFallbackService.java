caaae4c9e08e486a851743b8908f90f63f5dd9b9
chq <17875511931@163.com>
2020-07-13 15:11:12 +0800

    'sentinel整合ribbon+openfiegn+fallback'

---
+++ cloudalibaba-consumer-nacos-order84/src/main/java/com/chq/springcloud/service/PaymentFallbackService.java
@@ -0,0 +1,19 @@
+package com.chq.springcloud.service;
+
+
+import com.chq.springcloud.entities.CommonResult;
+import com.chq.springcloud.entities.Payment;
+import org.springframework.stereotype.Component;
+
+
+@Component
+public class PaymentFallbackService implements PaymentService
+{
+    @Override
+    public CommonResult<Payment> paymentSQL(Long id)
+    {
+        return new CommonResult<>(44444,"服务降级返回,---PaymentFallbackService",new Payment(id,"errorSerial"));
+    }
+}
+ 
+ 
