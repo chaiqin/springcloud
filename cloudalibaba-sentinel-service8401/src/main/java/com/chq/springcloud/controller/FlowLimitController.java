df804ef2ba3cc84ac964d7f3a92517c23f81b0a0
chq <17875511931@163.com>
2020-07-13 09:35:11 +0800

    'sentinel热点规则'

--- cloudalibaba-sentinel-service8401/src/main/java/com/chq/springcloud/controller/FlowLimitController.java
+++ cloudalibaba-sentinel-service8401/src/main/java/com/chq/springcloud/controller/FlowLimitController.java
@@ -1,7 +1,10 @@
 package com.chq.springcloud.controller;
 
+import com.alibaba.csp.sentinel.annotation.SentinelResource;
+import com.alibaba.csp.sentinel.slots.block.BlockException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.concurrent.TimeUnit;
@@ -27,6 +30,19 @@ public class FlowLimitController
         try { TimeUnit.SECONDS.sleep(1); } catch (InterruptedException e) { e.printStackTrace(); }
         log.info("testD 测试RT");
         return "------testD";
+    }
+
+    @GetMapping("/testHotKey")
+    @SentinelResource(value = "testHotKey",blockHandler = "deal_testHotKey")
+    public String testHotKey(@RequestParam(value = "p1",required = false) String p1,
+                             @RequestParam(value = "p2",required = false) String p2) {
+        //int age = 10/0;
+        return "------testHotKey";
+    }
+
+    //兜底方法
+    public String deal_testHotKey (String p1, String p2, BlockException exception){
+        return "------deal_testHotKey,o(╥﹏╥)o";
     }
 
 }
